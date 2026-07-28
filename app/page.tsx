aa346754f7e28d19a4c050eca6dbf80799ac1ee8
Shaunneh <shaunneh93@gmail.com>
2026-07-28 23:08:04 +0800

    Fix dynamic routing and update page config2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
-export const dynamic = 'force-dynamic';
 'use client';
+
+export const dynamic = 'force-dynamic';
+
 import { useState, useRef } from 'react';
 import { useLoadScript, Autocomplete } from '@react-google-maps/api';
 
